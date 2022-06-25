f3649558613754420fbe7587278b72b9cb791650
Ememfon <106170213+Ememfon@users.noreply.github.com>
2022-06-24 18:44:04 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-document.write["Name: Ememfon Inyang <br> Height:5ft8' <br> Country:Nigeria"]
+ const operator = prompt("Enter your op;eration tol perform the calculation(either +,-,* or /): ")
+ const number1 = parseFloat(prompt('Enter the first number: '));
+ const number2 = parseFloat(prompt('Enter the second number: '));
+ let result;
+
+ if (operator == '+') {
+    result = number1 + number2;
+ } else if (operator == '-') {
+    result = number1 - number2;
+ } else if (operator == '*') {
+    result = number1 * number2;
+ } else if (operator == '/') {
+    result = number1 / number2;
+ }
